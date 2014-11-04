42eea6332793896940afe01311a1fbb9b74c925d
Andrew Hallam <andrew.hallam.10@ucl.ac.uk>
2014-11-04 19:38:39 +0000

    Created points.py

---
+++ greengraph/points.py
@@ -0,0 +1,17 @@
+### "points"
+
+from numpy import linspace
+def location_sequence(start,end,steps):
+  # Would actually prefer this if steps
+  # were deduced from zoomlevel
+  # But need projection code for that
+  lats=linspace(start[0],end[0],steps)
+  longs=linspace(start[1],end[1],steps)
+  return zip(lats,longs)
+
+[count_green_in_png(map_at(*location,zoom=10,satellite=True))
+            for location in location_sequence(
+                geolocate("London"),
+                geolocate("Birmingham"),
+                10)]
+
