122f3da38b14fe982af9240544dfdbb396800597
Andrew Hallam <andrew.hallam.10@ucl.ac.uk>
2014-11-05 00:00:25 +0000

    Changed command.py to take in two arguments and create the appropriate graphs.

--- greengraph/command.py
+++ greengraph/command.py
@@ -1,1 +1,16 @@
-# 
+from argparse import ArgumentParser
+from main import plotgreengraph
+
+
+def process():
+   parser = ArgumentParser(description = "Look at green space between two locations.")
+
+   parser.add_argument('loc1')
+   parser.add_argument('--loc2')
+
+   arguments= parser.parse_args()
+   
+   plotgreengraph(arguments.loc1, arguments.loc2)
+   
+if __name__ == "__main__":
+    process() 
