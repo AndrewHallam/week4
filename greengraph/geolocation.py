8ecc09cfd9ca6d9de2af3ffe0df151650741b223
Andrew Hallam <andrew.hallam.10@ucl.ac.uk>
2014-11-05 02:57:25 +0000

    Changed the comments from geolocation.py

--- greengraph/geolocation.py
+++ greengraph/geolocation.py
@@ -1,4 +1,4 @@
-### "geolocation"
+### Geolocation will find the location of a string using Google Maps. 
 import geopy
 geocoder=geopy.geocoders.GoogleV3(domain="maps.google.co.uk")
 def geolocate(place):
