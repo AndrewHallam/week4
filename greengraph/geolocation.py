199018cf77ae0a98741f3ddc72652b26d5137a27
Andrew Hallam <andrew.hallam.10@ucl.ac.uk>
2014-11-05 02:37:02 +0000

    Made geolocator raise a type error if given the wrong things.

--- greengraph/geolocation.py
+++ greengraph/geolocation.py
@@ -2,4 +2,7 @@
 import geopy
 geocoder=geopy.geocoders.GoogleV3(domain="maps.google.co.uk")
 def geolocate(place):
+  geo1=geocoder.geocode(place,exactly_one=False)
+  if geo1 is None:
+     raise TypeError("This is not a location")
   return geocoder.geocode(place,exactly_one=False)[0][1]
